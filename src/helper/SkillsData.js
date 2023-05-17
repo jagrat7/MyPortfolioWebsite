fe7a66546a4d7ce40a1e37fd990eeb81ebc85ee9
jagrat7 <raojagrat@hotmail.com>
2023-05-17 02:31:43 -0400

    up

--- src/helper/SkillsData.js
+++ src/helper/SkillsData.js
@@ -1,12 +1,12 @@
 
 export const Technologies=[
-    "Angular", "React", "Nodejs", "Express", "AWS Cloud", "Django", "Git", "Github", "Gitlab", "MySQL", "Linux CLI", "Assembly","Embbeded Systems", "Oracle Database", "Trello",
+    "Angular", "React", "Nodejs", "Express", "AWS Cloud", "Django", "Git", "Github", "Gitlab", "MySQL", "Linux CLI",  "Oracle Database", "Trello",
                         "Spring boot","AJAX", "Pspice"
 
 ] ;
 export const Languages=[
 
-    "JavaScript", "Python", "Java", "MATLAB", "C", "C++", "HTML", "TypeScript", "CSS", "VHDL", "UML", "Verilog", "SQL", "PHP", "Solidity"
+    "JavaScript", "Python", "Java", "MATLAB", "C", "C++", "HTML", "TypeScript", "CSS", "VHDL", "UML", "Verilog", "SQL"
 ] ;
 export const Other=[
 
